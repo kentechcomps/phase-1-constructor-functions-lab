f460bb845024bae6efffaa3da8f018f1353dbc8f
user <mutukukennedy34@gmail.com>
2023-07-13 15:11:18 +0300

    constructor

---
+++ index.js
@@ -0,0 +1,25 @@
+
+class cooter {
+    constructor(year, color, model) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+}
+    //Driver with name, age, and experience properties
+
+    class Driver{
+        constructor(name , age , experience){
+            this.name = name 
+            this.age = age
+            this.experience = experience
+        }
+    }
+    //pickuplocation with address and city
+    class pickuplocation{
+        constructor(address , city){
+     this.address = address
+        this.city = city
+        }
+     
+    }
